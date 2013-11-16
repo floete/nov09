2e0a65c5a4e468568f3e4ebe2d0d4093f778a1e7
floete <bemmannqiq@gmail.com>
2013-11-16 17:38:09 +0900

    Added Error-Checking when opening a file.

--- mix3.c
+++ mix3.c
@@ -18,15 +18,22 @@ int main(int argc, char* argv[])
 {
 	if(argc != 3)
 	{
-		printf("引数の数が不適切です。\n");
-		printf("./a.exe fileA.txt, fileB.txt \n");
+		fprintf(stderr,"引数の数が不適切です。\n");
+		fprintf(stderr,"./a.exe fileA.txt, fileB.txt \n");
 		return -1;
 	}
 	
 	FILE *fpA, *fpB;
-	fpA = fopen( argv[1], "r" );
-	fpB = fopen( argv[2], "r" );
-	
+	if( (fpA = fopen( argv[1], "r" ) ) == NULL )
+	{
+		fprintf(stderr,"%sは開けません。\n",argv[1]);
+		return -1;
+	}
+	if( (fpB = fopen( argv[2], "r" ) ) == NULL )
+	{
+		fprintf(stderr,"%sは開けません。\n",argv[2]);
+		return -1;
+	}
 	int i;
 	while(1)
 	{
